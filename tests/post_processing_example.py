0ff78c5a26ad06f71d7a7b398daf12ea60a88db1
b-reyes <reye112@we37216.pnl.gov>
2019-09-30 17:40:24 -0700

    updating post processing ex

--- tests/post_processing_example.py
+++ tests/post_processing_example.py
@@ -25,26 +25,16 @@ multistable_param_ind = opt.run_greedy_continuity_analysis(species="s15", parame
 
 opt.generate_report()
 
-odes = opt.get_independent_odes()
-
 import sympy
 
 print("Original ODEs")
-sympy.pprint(network.get_c_graph().get_ode_system())
-print("")
-print("Independent ODEs")
+odes = network.get_c_graph().get_ode_system()
 sympy.pprint(odes)
-
-species = opt.get_independent_species()
-
-print("Independent Species")
-print(species)
 
 decision_vector_values = params_for_global_min[2]
 
 species_concentrations = []
 for i in opt.get_concentration_funs():
-
     species_concentrations.append(i(*tuple(decision_vector_values)))
 
 print("species_concentrations")
@@ -57,86 +47,106 @@ conservation_values = network.get_c_graph().get_b()*sympy.Matrix([species_concen
 print("conservation_values")
 print(conservation_values)
 
-#creating the input for each ode lambda function
-lambda_inputs = [sympy.Symbol(i, positive=True) for i in network.get_c_graph().get_reactions()] + species + \
-                [sympy.Symbol('C1', real=True), sympy.Symbol('C2', real=True), sympy.Symbol('C3', real=True)]
+print("conservation laws")
+print(opt.get_conservation_laws())
 
+print("decision vector")
+print(decision_vector_values)
+
+# construct sympy form of reactions and species
+sympy_reactions = [sympy.Symbol(i, positive=True) for i in network.get_c_graph().get_reactions()]
+sympy_species = [sympy.Symbol(i, positive=True) for i in network.get_c_graph().get_species()]
+
+# creating the input for each ode lambda function
+lambda_inputs = sympy_reactions + sympy_species
+
+print("lambda_inputs")
 print(lambda_inputs)
 
-#creating a lambda function for each ODE to
+# creating a lambda function for each ODE to
 ode_lambda_functions = []
 for i in range(len(odes)):
     ode_lambda_functions += [sympy.utilities.lambdify(lambda_inputs, odes[i])]
 
+# filling in the values for the reaction rates using optimization values
 input_vals = numpy.zeros(len(lambda_inputs))
-input_vals[0] = decision_vector_values[0]
-input_vals[1] = decision_vector_values[1]
-input_vals[2] = decision_vector_values[2]
-input_vals[3] = decision_vector_values[3]
-input_vals[4] = decision_vector_values[4]
-input_vals[5] = decision_vector_values[5]
-input_vals[6] = decision_vector_values[6]
-input_vals[7] = decision_vector_values[7]
-input_vals[8] = decision_vector_values[8]
-
-input_vals[13] = conservation_values[0]
-input_vals[14] = conservation_values[1]
-#input_vals[15] = 877876.0
-#input_vals[15] = 878752.0
-#input_vals[15] = conservation_values[2]
+for i in range(len(sympy_reactions)):
+    input_vals[i] = decision_vector_values[i]
 
 # solve the system dy/dt = f(y, t)
-def f(y, t, inputs, ode_lambda_func):
-    # s15 = y[0]
-    # s3 = y[1]
-    # s6 = y[2]
-    # s16 = y[3]
+def f(y, t, inputs, ode_lambda_func, start_ind):
 
-    inputs[9] = y[0]#s15
-    inputs[10] = y[1]#s3
-    inputs[11] = y[2]#s6
-    inputs[12] = y[3]#s16
+    # setting species concentrations
+    ind = start_ind
+    for i in y:
+        inputs[ind] = i
+        ind += 1
 
     # the model equations
-    f0 = ode_lambda_func[0](*tuple(inputs))
-    f1 = ode_lambda_func[1](*tuple(inputs))
-    f2 = ode_lambda_func[2](*tuple(inputs))
-    f3 = ode_lambda_func[3](*tuple(inputs))
+    ode_vals = []
+    for i in ode_lambda_func:
+        ode_vals.append(i(*tuple(inputs)))
 
-    return [f0, f1, f2, f3]
+    return ode_vals
 
-# time grid
-t = numpy.linspace(1, 20., 10000)
 
-#print(t)
+# first index of a species in
+start_index = len(sympy_reactions)
 
 # solve the ODEs
 import scipy.integrate
-
-# initial conditions
-#y0 = [species_concentrations[6], species_concentrations[2], species_concentrations[3], species_concentrations[5]]
-y0 = [34793.6, species_concentrations[2], species_concentrations[3], species_concentrations[5]]
-input_vals[15] = 877876.0
-#y0 = [60000.0, species_concentrations[2], species_concentrations[3], species_concentrations[5]]
-
-soln = scipy.integrate.odeint(f, y0, t, args=(input_vals, ode_lambda_functions))
-
-s15_sol1 = soln[:, 0]
-
-y0 = [122611.0, species_concentrations[2], species_concentrations[3], species_concentrations[5]]
-input_vals[15] = 878752.0
-#y0 = [60000.0, species_concentrations[2], species_concentrations[3], species_concentrations[5]]
-
-soln2 = scipy.integrate.odeint(f, y0, t, args=(input_vals, ode_lambda_functions))
-
-s15_sol2 = soln2[:, 0]
 
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 
+# time grid
+t = numpy.linspace(0.0, 10.0, 10000)
 
-plt.plot(t, s15_sol1, label='Species s15 1')
-plt.plot(t, s15_sol2, label='Species s15 2')
-plt.ticklabel_format(axis='both', style='sci', scilimits=(-2, 2))
-plt.show()
+y0 = [10000.0, 10000.0, 10000.0, 10000.0, 10000.0, 10000.0, 10000.0]
+soln1 = scipy.integrate.odeint(f, y0, t, args=(input_vals, ode_lambda_functions, start_index))
+
+s1_sol = soln1[:, 0]
+s2_sol = soln1[:, 1]
+s3_sol = soln1[:, 2]
+s6_sol = soln1[:, 3]
+s7_sol = soln1[:, 4]
+s16_sol = soln1[:, 5]
+s15_sol = soln1[:, 6]
+
+plt.plot(t, s1_sol, label='Species s1')
+plt.plot(t, s2_sol, label='Species s2')
+plt.plot(t, s3_sol, label='Species s3')
+plt.plot(t, s6_sol, label='Species s6')
+plt.plot(t, s7_sol, label='Species s7')
+plt.plot(t, s16_sol, label='Species s16')
+plt.plot(t, s15_sol, label='Species s15')
+plt.legend(loc='upper right')
+plt.xlabel("time")
+plt.ylabel("Species' Concentrations")
+plt.title("Toy Values")
+
+plt.savefig('./num_cont_graphs/toy_values.png')
+plt.clf()
+
+# time grid
+t = numpy.linspace(0.0, 20000.0, 1000000)
+
+y0 = [877900.0, 760.070288586031, 0.0, 0.0, 481700.406580797, 0.0, 0.0]
+soln1 = scipy.integrate.odeint(f, y0, t, args=(input_vals, ode_lambda_functions, start_index))
+s15_sol1 = soln1[:, 6]
+
+y0 = [878400.0, 760.070288586031, 0.0, 0.0, 481700.406580797, 0.0, 0.0]
+soln2 = scipy.integrate.odeint(f, y0, t, args=(input_vals, ode_lambda_functions, start_index))
+s15_sol2 = soln2[:, 6]
+
+
+plt.plot(t, s15_sol1, label='s1 = 877900.0')
+plt.plot(t, s15_sol2, label='s1 = 878400.0')
+plt.xlabel("time")
+plt.ylabel("Concentration of s15")
+plt.legend(loc="upper right")
+plt.title("Initial Values from Optimization")
+
+plt.savefig('./num_cont_graphs/optimization_values.png')
+plt.clf()
