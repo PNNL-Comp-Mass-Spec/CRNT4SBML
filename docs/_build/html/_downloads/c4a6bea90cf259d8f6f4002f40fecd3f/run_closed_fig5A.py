898cba22c0d540245dd322a33ae92963db5b7384
b-reyes <reye112@we37216.pnl.gov>
2019-10-23 15:00:33 -0700

    updating docs on the user case scenario

---
+++ docs/_build/html/_downloads/c4a6bea90cf259d8f6f4002f40fecd3f/run_closed_fig5A.py
@@ -0,0 +1,34 @@
+import crnt4sbml
+
+network = crnt4sbml.CRNT("../sbml_files/closed_fig5A.xml")
+
+network.basic_report()
+
+network.print_c_graph()
+
+ldt = network.get_low_deficiency_approach()
+ldt.report_deficiency_zero_theorem()
+ldt.report_deficiency_one_theorem()
+
+print("")
+
+opt = network.get_mass_conservation_approach()
+
+print("Decision Vector:")
+print(opt.get_decision_vector())
+print("")
+
+print("Species for concentration bounds:")
+print(opt.get_concentration_bounds_species())
+
+bounds = [(1e-2, 1e2)]*12
+concentration_bounds = [(1e-2, 1e2)]*6
+
+params_for_global_min, obj_fun_val_for_params = opt.run_optimization(bounds=bounds,
+                                                                     concentration_bounds=concentration_bounds,
+                                                                     iterations=100)
+
+multistable_param_ind, plot_specifications = opt.run_continuity_analysis(species="s9", parameters=params_for_global_min,
+                                                                         auto_parameters={'PrincipalContinuationParameter': 'C2'})
+
+opt.generate_report()
---
+++ docs/_build/html/_downloads/d19062b60d68b9bb404fadfbeca5fc23/run_fig1Aii.py
@@ -0,0 +1,14 @@
+import crnt4sbml
+
+network = crnt4sbml.CRNT("../sbml_files/Fig_1Aii.xml")
+
+network.basic_report()
+
+network.print_c_graph()
+
+ldt = network.get_low_deficiency_approach()
+ldt.report_deficiency_zero_theorem()
+ldt.report_deficiency_one_theorem()
+
+opt = network.get_mass_conservation_approach()
+
